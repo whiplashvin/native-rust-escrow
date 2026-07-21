5fe365b2470d4a3e838f7e7106ecba9996ef60b1
arvindkhoisnam <khoisnamarvind@gmail.com>
2026-07-22 00:47:22 +0530

    adding-more-tests

--- client/index.test.ts
+++ client/index.test.ts
@@ -8,12 +8,27 @@ import {
   pipe,
   type Address,
   type KeyPairSigner,
+  getStructDecoder,
+  getBooleanDecoder,
+  getAddressDecoder,
+  getU64Decoder,
+  getU64Encoder,
+  AccountRole,
+  address,
+  getProgramDerivedAddress,
 } from "@solana/kit";
 import { getCreateAccountInstruction } from "@solana-program/system";
 import {
   TOKEN_PROGRAM_ADDRESS,
   getMintSize,
   getInitializeMint2Instruction,
+  getCreateAssociatedTokenInstruction,
+  findAssociatedTokenPda,
+  getMintToInstruction,
+  getTokenDecoder,
+  getTransferInstruction,
+  getTokenSize,
+  getInitializeAccount3Instruction,
 } from "@solana-program/token";
 import { describe, test, expect, beforeAll } from "bun:test";
 import { LiteSVM, FailedTransactionMetadata } from "litesvm";
@@ -29,26 +44,35 @@ let svm: LiteSVM;
 let programId: Address;
 let initializer: KeyPairSigner;
 let mintX: KeyPairSigner;
+let mintY: KeyPairSigner;
 let initializerTokenXAcc: Address;
-let initializerTempTokenXAcc: Address;
-let escrowAcc: Address;
+let initializerTokenYAcc: Address;
+let initializerTempTokenXAcc: KeyPairSigner;
+let escrowAcc: KeyPairSigner;
 
 beforeAll(async () => {
   svm = new LiteSVM();
   programId = (await generateKeyPairSigner()).address;
   initializer = await generateKeyPairSigner();
+  initializerTempTokenXAcc = await generateKeyPairSigner();
+  escrowAcc = await generateKeyPairSigner();
   svm.airdrop(initializer.address, lamports(10_000_000_000n));
 
   mintX = await generateKeyPairSigner();
+  mintY = await generateKeyPairSigner();
   const space = BigInt(getMintSize()); // 82 bytes
+  const tokenAccSpace = BigInt(getTokenSize());
+  const escrowStateSpace = 105n;
   const rent = svm.minimumBalanceForRentExemption(space);
+  const rentForTokAcc = svm.minimumBalanceForRentExemption(tokenAccSpace);
+  const rentForEscrowAcc = svm.minimumBalanceForRentExemption(escrowStateSpace);
 
   const createMintAccountIx = getCreateAccountInstruction({
     payer: initializer,
-    newAccount: mintX, // the mint is a signer: it signs its own creation
+    newAccount: mintX,
     lamports: lamports(rent),
     space,
-    programAddress: TOKEN_PROGRAM_ADDRESS, // mint account is owned by the token program
+    programAddress: TOKEN_PROGRAM_ADDRESS,
   });
 
   const initMintIx = getInitializeMint2Instruction({
@@ -58,13 +82,104 @@ beforeAll(async () => {
     freezeAuthority: null,
   });
 
+  const [initializerTokenX] = await findAssociatedTokenPda({
+    owner: initializer.address,
+    mint: mintX.address,
+    tokenProgram: TOKEN_PROGRAM_ADDRESS,
+  });
+  initializerTokenXAcc = initializerTokenX;
+
+  const tokenXAccIx = getCreateAssociatedTokenInstruction({
+    payer: initializer,
+    ata: initializerTokenXAcc,
+    owner: initializer.address,
+    mint: mintX.address,
+  });
+
+  const mintXToAtaIx = getMintToInstruction({
+    mint: mintX.address,
+    token: initializerTokenXAcc,
+    mintAuthority: initializer,
+    amount: 100n,
+  });
+
+  const createMintYAccountIx = getCreateAccountInstruction({
+    payer: initializer,
+    newAccount: mintY,
+    lamports: lamports(rent),
+    space,
+    programAddress: TOKEN_PROGRAM_ADDRESS,
+  });
+
+  const initMintYIx = getInitializeMint2Instruction({
+    mint: mintY.address,
+    decimals: 9,
+    mintAuthority: initializer.address,
+    freezeAuthority: null,
+  });
+
+  const [initializerTokenY] = await findAssociatedTokenPda({
+    owner: initializer.address,
+    mint: mintY.address,
+    tokenProgram: TOKEN_PROGRAM_ADDRESS,
+  });
+  initializerTokenYAcc = initializerTokenY;
+
+  const tokenYAccIx = getCreateAssociatedTokenInstruction({
+    payer: initializer,
+    ata: initializerTokenYAcc,
+    owner: initializer.address,
+    mint: mintY.address,
+  });
+
+  const createInitializerTempAccountIx = getCreateAccountInstruction({
+    payer: initializer,
+    newAccount: initializerTempTokenXAcc,
+    lamports: lamports(rentForTokAcc),
+    space: tokenAccSpace,
+    programAddress: TOKEN_PROGRAM_ADDRESS,
+  });
+
+  const initTempAccountIx = getInitializeAccount3Instruction({
+    account: initializerTempTokenXAcc.address,
+    mint: mintX.address,
+    owner: initializer.address,
+  });
+
+  const transferXtoTempIx = getTransferInstruction({
+    source: initializerTokenXAcc,
+    destination: initializerTempTokenXAcc.address,
+    authority: initializer,
+    amount: 100n,
+  });
+
+  const createEscorwIx = getCreateAccountInstruction({
+    payer: initializer,
+    newAccount: escrowAcc,
+    lamports: lamports(rentForEscrowAcc),
+    space: escrowStateSpace,
+    programAddress: programId,
+  });
+
   const message = pipe(
     createTransactionMessage({ version: 0 }),
     (m) => setTransactionMessageFeePayerSigner(initializer, m),
     (m) => svm.setTransactionMessageLifetimeUsingLatestBlockhash(m),
     (m) =>
       appendTransactionMessageInstructions(
-        [createMintAccountIx, initMintIx],
+        [
+          createMintAccountIx,
+          initMintIx,
+          tokenXAccIx,
+          mintXToAtaIx,
+          createMintYAccountIx,
+          initMintYIx,
+          tokenYAccIx,
+          createInitializerTempAccountIx,
+          initTempAccountIx,
+          transferXtoTempIx,
+          createEscorwIx,
+        ],
         m,
       ),
   );
@@ -73,7 +188,7 @@ beforeAll(async () => {
   const result = svm.sendTransaction(signedTx);
 
   if (result instanceof FailedTransactionMetadata) {
-    throw new Error("mint creation failed: " + result.err().toString());
+    throw new Error(result.err().toString());
   }
 });
 
@@ -84,4 +199,106 @@ test("mint X is created and owned by the token program", () => {
     expect(mintAccount.programAddress).toBe(TOKEN_PROGRAM_ADDRESS);
     expect(mintAccount.data.length).toBe(getMintSize());
   }
+});
+
+test("check ata", () => {
+  const mintAccount = svm.getAccount(mintX.address);
+  const ata = svm.getAccount(initializerTokenXAcc);
+  expect(ata.exists).toBe(true);
+  if (!ata.exists) return;
+  const decoded = getTokenDecoder().decode(ata.data);
+  expect(decoded.mint).toBe(mintAccount.address);
+  expect(decoded.owner).toBe(initializer.address);
+  expect(decoded.amount).toBe(0n);
+});
+
+test("check temp X token account", () => {
+  const mintAccount = svm.getAccount(mintX.address);
+  const temp = svm.getAccount(initializerTempTokenXAcc.address);
+  expect(temp.exists).toBe(true);
+  if (!temp.exists) return;
+  const decoded = getTokenDecoder().decode(temp.data);
+  expect(decoded.mint).toBe(mintAccount.address);
+  expect(decoded.owner).toBe(initializer.address);
+  expect(decoded.amount).toBe(100n);
+});
+
+test("create escrow account", () => {
+  let escrowAccount = svm.getAccount(escrowAcc.address);
+  expect(escrowAccount.exists).toBe(true);
+  if (!escrowAccount.exists) return;
+  const escrowDecoder = getStructDecoder([
+    ["isInitialized", getBooleanDecoder()],
+    ["initializerPubkey", getAddressDecoder()],
+    ["tempTokenAccountPubkey", getAddressDecoder()],
+    ["initializerTokenToReceiveAccountPubkey", getAddressDecoder()],
+    ["expectedAmount", getU64Decoder()],
+  ]);
+  const decoded = escrowDecoder.decode(escrowAccount.data);
+});
+
+test("initEscrow", async () => {
+  svm.addProgramFromFile(programId, "../target/deploy/native_rust_escrow.so");
+  const RENT_SYSVAR = address("SysvarRent111111111111111111111111111111111");
+  const expectedAmount = 50n;
+  const data = new Uint8Array([0, ...getU64Encoder().encode(expectedAmount)]);
+  const initEscrowIx = {
+    programAddress: programId,
+    accounts: [
+      { address: initializer.address, role: AccountRole.READONLY_SIGNER },
+      { address: initializerTempTokenXAcc.address, role: AccountRole.WRITABLE },
+      { address: initializerTokenYAcc, role: AccountRole.READONLY },
+      { address: escrowAcc.address, role: AccountRole.WRITABLE },
+      { address: RENT_SYSVAR, role: AccountRole.READONLY },
+      { address: TOKEN_PROGRAM_ADDRESS, role: AccountRole.READONLY },
+    ],
+    data,
+  };
+
+  const message = pipe(
+    createTransactionMessage({ version: 0 }),
+    (m) => setTransactionMessageFeePayerSigner(initializer, m),
+    (m) => svm.setTransactionMessageLifetimeUsingLatestBlockhash(m),
+    (m) => appendTransactionMessageInstructions([initEscrowIx], m),
+  );
+
+  const signedTx = await signTransactionMessageWithSigners(message);
+  const result = svm.sendTransaction(signedTx);
+  if (result instanceof FailedTransactionMetadata) {
+    throw new Error(result.err().toString());
+  }
+
+  const escrowAccount = svm.getAccount(escrowAcc.address);
+  expect(escrowAccount.exists).toBe(true);
+  if (!escrowAccount.exists) return;
+
+  const escrowDecoder = getStructDecoder([
+    ["isInitialized", getBooleanDecoder()],
+    ["initializerPubkey", getAddressDecoder()],
+    ["tempTokenAccountPubkey", getAddressDecoder()],
+    ["initializerTokenToReceiveAccountPubkey", getAddressDecoder()],
+    ["expectedAmount", getU64Decoder()],
+  ]);
+  const escrow = escrowDecoder.decode(escrowAccount.data);
+  console.log(escrow);
+  expect(escrow.isInitialized).toBe(true);
+  expect(escrow.initializerPubkey).toBe(initializer.address);
+  expect(escrow.tempTokenAccountPubkey).toBe(initializerTempTokenXAcc.address);
+  expect(escrow.initializerTokenToReceiveAccountPubkey).toBe(
+    initializerTokenYAcc,
+  );
+  expect(escrow.expectedAmount).toBe(expectedAmount);
+
+  const [escrowPda] = await getProgramDerivedAddress({
+    programAddress: programId,
+    seeds: [new TextEncoder().encode("escrow")],
+  });
+
+  const temp = svm.getAccount(initializerTempTokenXAcc.address);
+
+  expect(temp.exists).toBe(true);
+  if (!temp.exists) return;
+  const tempDecoded = getTokenDecoder().decode(temp.data);
+  expect(tempDecoded.owner).toBe(escrowPda);
+  expect(tempDecoded.amount).toBe(100n);
 });
